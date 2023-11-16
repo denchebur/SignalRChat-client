89493f06d0a54c7ff9aa515ae77a6fe980f230ee
denchebur <denis.tcheberiak@gmail.com>
2023-11-16 09:00:44 +0200

    try-add-front

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import * as signalR from '@microsoft/signalr';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+
+class Chat extends Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+      newMessage: '',
+    };
+    this.connection = new signalR.HubConnectionBuilder()
+      .withUrl('https://localhost:7203/api/chatHub' , {
+        
+      transport: signalR.HttpTransportType.WebSockets,
+      
+        
+    }) 
+      .build();
+
+    this.connection.on('Receive', (message) => {
+      this.setState((prevState) => ({
+        messages: [...prevState.messages, message],
+      }));
+    });
+  }
+
+  componentDidMount() {
+    if (this.connection.state === 'Disconnected') {
+      this.connection.start().catch((error) => console.error(error));
+    }
+  }
+
+  handleNewMessageChange = (event) => {
+    this.setState({ newMessage: event.target.value });
+  };
+
+  sendMessage = () => {
+    const { newMessage } = this.state;
+    if (newMessage.trim() === '') return;
+
+    this.connection
+      .invoke('Send', newMessage)
+      .catch((error) => console.error(error));
+
+    this.setState({ newMessage: '' });
+  };
+
+  render() {
+    const { messages, newMessage } = this.state;
+
+    return (
+      <div className="chat">
+        <div className="message-list">
+          {messages.map((message, index) => (
+            <div key={index} className="message">
+              {message}
+            </div>
+          ))}
+        </div>
+        <div className="message-input">
+          <input
+            type="text"
+            placeholder="Введите сообщение"
+            value={newMessage}
+            onChange={this.handleNewMessageChange}
+          />
+          <button onClick={this.sendMessage}>Отправить</button>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Chat;
